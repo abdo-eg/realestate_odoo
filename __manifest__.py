2ed7051f990d8e7d20fb709bd0f596d3bf0edcc0
abdo-eg <alsharifegy@gmail.com>
2024-11-19 19:05:26 +0200

    add core program and set the basic fields

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Realestate',
+    'version': '17.0.0.1.0',
+    'category': 'learning_modules',
+    'author': 'Abdullah Ahmed',
+    'summary': 'simple realestate application',
+
+    'depends': [
+        'base',
+    ],
+    'data': [
+
+
+    ],
+
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
